22ca7e2b46696b6897196935c1d8fcc5b4dac090
merolley <merolly.caay11@gmail.com>
2024-10-01 15:40:41 +0800

    initial commit

---
+++ evenNumbers.js
@@ -0,0 +1,11 @@
+/*references:
+ https://www.w3schools.com/js/js_loop_for.asp
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration
+*/
+//loop that will print the even numbers from 1-10
+let i;
+for (i = 1; i < 11; i++){
+    if (i % 2 === 0) {
+    console.log(i);
+    }
+}
